43be39c33c3c9da298262bcf96612b1162665b63
lakshyarawat1 <lakshya.paramount@gmail.com>
2023-06-02 22:14:34 +0530

    .

--- transcribia_app/streamlit_app.py
+++ transcribia_app/streamlit_app.py
@@ -42,10 +42,6 @@ with c32:
     st.caption("")
     st.caption("")
 
-    st.write(
-        "&nbsp &nbsp Made in [![this is an image link](https://i.imgur.com/iIOA6kU.png)](https://www.streamlit.io/)&nbsp, with :heart: by [@DataChaz](https://www.charlywargnier.com/) | [![this is an image link](https://i.imgur.com/thJhzOO.png)](https://www.buymeacoffee.com/cwar05)"
-    )
-
 st.text("")
 st.markdown(
     f"""
